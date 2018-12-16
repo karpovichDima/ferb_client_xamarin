8667c8a42d12d163287c1c9d6208c8bc0d06edf9
Karpovich Dima <karpovichdi@hotmail.com>
2018-12-16 12:31:05 +0300

    basic login page with basic validation

---
+++ App1/App1/Models/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Models
+{
+    class Constants
+    {
+        public static bool isDev = true;
+    }
+}
---
+++ App1/App1/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Models
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User(){}
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public bool CheckInformation()
+        {
+            if (!this.Username.Equals("") && !this.Password.Equals(""))
+                return true;
+            else
+                return false;
+        }
+    }
+}
